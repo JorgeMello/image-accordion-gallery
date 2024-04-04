dbe596b9bc4a9e2fb3b8e700ebf7e74a5f09daaf
Jorge MSMF <jorge@feiradosautomoveis.com.br>
2024-04-04 17:06:39 -0300

    Alterações nas Rotas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,5 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->post('/incluirimagem', 'IncluirImagem::index');
+$routes->get('/excluirimagem', 'ExcluirImagem::index');
+
